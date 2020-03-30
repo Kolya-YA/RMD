f97d56f199a053cef3458d7995616b557921864b
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2020-03-30 17:23:14 +0200

    update Actions

--- themes/rmd-theme/assets/js/main.js
+++ themes/rmd-theme/assets/js/main.js
@@ -1,1 +1,136 @@
-!function(e){var t={};function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)n.d(o,r,function(t){return e[t]}.bind(null,r));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){e.exports=n(1)},function(e,t,n){"use strict";n.r(t);(function(){const e=document.getElementById("nav-toggle-open"),t=document.getElementById("nav-toggle-close"),n=document.getElementById("nav-wrapper");e.addEventListener("click",()=>{n.classList.remove("top-menu--close")},{passive:!0}),t.addEventListener("click",()=>{n.classList.add("top-menu--close")},{passive:!0})})(),function(){const e=document.getElementById("back-to-top");e.addEventListener("click",()=>{window.scrollTo({top:0,left:0,behavior:"smooth"})},{passive:!0});window.addEventListener("scroll",()=>{window.scrollY>=150?e.classList.remove("back-to-top--hidden"):e.classList.add("back-to-top--hidden")},{passive:!0})}()}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./themes/rmd-theme/src/js/back-to-top.js":
+/*!************************************************!*\
+  !*** ./themes/rmd-theme/src/js/back-to-top.js ***!
+  \************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ((function() {\n  const backToTopButton = document.getElementById('back-to-top');\n  \n  backToTopButton.addEventListener(\n    'click',\n    () => {window.scrollTo({\n        top: 0,\n        left: 0,\n        behavior: 'smooth'\n      })\n    },\n    { passive: true }\n  )\n  \n  const toggleToTop = () => { \n    let y = window.scrollY;\n    if ( y >= 150 ) {\n      backToTopButton.classList.remove('back-to-top--hidden')\n    } else {\n      backToTopButton.classList.add('back-to-top--hidden')\n    }\n  }\n  window.addEventListener( 'scroll', toggleToTop, { passive: true })\n\n  })());\n\n//# sourceURL=webpack:///./themes/rmd-theme/src/js/back-to-top.js?");
+
+/***/ }),
+
+/***/ "./themes/rmd-theme/src/js/nav-toggler.js":
+/*!************************************************!*\
+  !*** ./themes/rmd-theme/src/js/nav-toggler.js ***!
+  \************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ((function () {\n  const navOpenButton = document.getElementById('nav-toggle-open');\n  const navCloseButton = document.getElementById('nav-toggle-close');\n  const navWrapper = document.getElementById('nav-wrapper');\n\n  navOpenButton.addEventListener(\n    'click',\n    () => {\n      navWrapper.classList.remove('top-menu--close')\n    }, {\n      passive: true\n    }\n  );\n\n  navCloseButton.addEventListener(\n    'click',\n    () => {\n      navWrapper.classList.add('top-menu--close')\n    }, {\n      passive: true\n    }\n  );\n})());\n\n//# sourceURL=webpack:///./themes/rmd-theme/src/js/nav-toggler.js?");
+
+/***/ }),
+
+/***/ "./themes/rmd-theme/src/js/start.js":
+/*!******************************************!*\
+  !*** ./themes/rmd-theme/src/js/start.js ***!
+  \******************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _nav_toggler__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./nav-toggler */ \"./themes/rmd-theme/src/js/nav-toggler.js\");\n/* harmony import */ var _back_to_top__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./back-to-top */ \"./themes/rmd-theme/src/js/back-to-top.js\");\n// <== === nav toggler === ==>\n\n    \n// <== === back to top === ==>\n\n\n// <== === current active menu === ==>\n\n// const lastActive = document.querySelectorAll('.active')\n// lastActive.length && console.log(\"__Active: \", lastActive[lastActive.length - 1].textContent)\n\n//# sourceURL=webpack:///./themes/rmd-theme/src/js/start.js?");
+
+/***/ }),
+
+/***/ 0:
+/*!************************************************!*\
+  !*** multi ./themes/rmd-theme/src/js/start.js ***!
+  \************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__(/*! /Users/kolya_ya/Hugo-One/RMD/rmd-bau/themes/rmd-theme/src/js/start.js */\"./themes/rmd-theme/src/js/start.js\");\n\n\n//# sourceURL=webpack:///multi_./themes/rmd-theme/src/js/start.js?");
+
+/***/ })
+
+/******/ });
